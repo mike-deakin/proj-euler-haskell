1d324dd2604c0422e26b341c0bb5d99171e4d5e4
Mike Deakin <madeakin93@gmail.com>
2023-07-09 22:30:05 +0100

    Solution to problem 4 partially optimised

--- src/Problem004.hs
+++ src/Problem004.hs
@@ -4,15 +4,28 @@ module Problem004 where
 
 import Solutions
 import Data.Function
+import Control.Applicative
 
-solution :: Solution Int Int
-solution Nothing = solution (Just 3)
-solution (Just n) = (*) <$> [maxn,maxn-1..1] <*> [maxn,maxn-1..1]
+naiveSoln :: Int -> Int
+naiveSoln n = liftA2 (*) [maxn,maxn-1..100] [maxn,maxn-1..100]
     & map show
      & filter isPalindrome
      & map (read :: String -> Int)
      & maximum
     where maxn = 10^n - 1
+
+optimisedSoln :: Int -> Int
+optimisedSoln 3 =
+    [x*y | x <- [990::Int,979..110], y <- [999,998..x]]
+    & map show
+    & filter isPalindrome
+    & map (read :: String -> Int)
+    & maximum
+optimisedSoln _ = error "Only optimised for n = 3 case!"
+
+solution :: Solution Int Int
+solution Nothing = optimisedSoln 3
+solution (Just n) = naiveSoln n
 
 isPalindrome :: String -> Bool
 isPalindrome [] = True
